e97fcc32ab6a7e08e955f1f3fe2f17a6ee705056
Gaurav4050 <gauravsinghrathorerathore@gmail.com>
2026-04-03 23:15:41 +0530

    feat: add contact routes to API

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const connectDB = require('./config/db');
 const appointmentRoutes = require('./routes/appointmentRoutes');
 const authRoutes = require('./routes/authRoutes');
 const doctorRoutes = require('./routes/doctorRoutes');
+const contactRoutes = require('./routes/contactRoutes');
 
 // Connect Database
 connectDB();
@@ -32,6 +33,7 @@ app.use(limiter);
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/doctors', doctorRoutes);
+app.use('/api/contact', contactRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
